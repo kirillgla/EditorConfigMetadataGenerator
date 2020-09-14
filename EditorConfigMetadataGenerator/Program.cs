9f1d3c9407b9e7f0145741f0fc9f485ae893910b
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-09-14 14:49:56 +0300

    Initial commit

---
+++ EditorConfigMetadataGenerator/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EditorConfigMetadataGenerator
+{
+    internal static class Program
+    {
+        private const string DefaultDataLocation = @"C:\w\EditorConfigMetadataGenerator\EditorConfigMetadataGenerator";
+        private static readonly Regex TagRegex = new Regex("</?[^<>]+>");
+
+        private static readonly Regex RuleRegex = new Regex(@"<h[45] id=""(\w|_)+"">(?<ruleName>(\w|_)+)</h[45]>(
+<p>(?<ruleDocumentation>.+)</p>)?
+<table>
+<thead>
+<tr>
+<th>Property</th>
+<th>Value</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><strong>Rule name</strong></td>
+<td>(\w|_)+</td>
+</tr>
+<tr>
+<td><strong>Applicable languages</strong></td>
+<td>.+</td>
+</tr>(
+<tr>
+<td><strong>Introduced version</strong></td>
+<td>.+</td>
+</tr>)?
+<tr>
+<td><strong>Values</strong></td>
+<td>(?<ruleValues>.+)</td>
+</tr>
+</tbody>
+</table>");
+
+        private static readonly Regex RuleFirstValueRegex =
+            new Regex(@"^<code>(?<valueName>(\w)+)</code> -(?<documentation>([^<]|<[^b]|<b[^r]|<br[^>])*)<br><br>");
+
+        private static readonly Regex RuleValueRegex =
+            new Regex(@"<br><br><code>(?<valueName>(\w)+)</code> -(?<documentation>([^<]|<[^b]|<b[^r]|<br[^>])*)");
+
+        static void Main(string[] args)
+        {
+            string dataLocation = GetDataLocation(args);
+            string text = File.ReadAllText($@"{dataLocation}\Data.html");
+            using var writer = new StreamWriter($@"{dataLocation}\Output.json");
+            var optionsMatches = RuleRegex.Matches(text);
+
+            Console.WriteLine($"Total rules found: {optionsMatches.Count}");
+
+            writer.WriteLine("[");
+            int currentIndex = 0;
+            foreach (Match optionMatch in optionsMatches)
+            {
+                writer.WriteLine("  {");
+                writer.WriteLine("    \"type\": \"option\",");
+                WriteKey(writer, optionMatch);
+                writer.WriteLine("    \"value\": {");
+                writer.WriteLine("      \"type\": \"union\",");
+                writer.WriteLine("      \"values\": [");
+                WriteValues(optionMatch, writer);
+                writer.WriteLine("      ]");
+                writer.WriteLine("    }");
+                writer.Write("  }");
+                writer.WriteLine(currentIndex == optionsMatches.Count - 1 ? "" : ",");
+                currentIndex += 1;
+            }
+
+            writer.WriteLine("]");
+        }
+
+        private static void WriteValues(Match optionMatch, StreamWriter writer)
+        {
+            string rawRuleValues = optionMatch.Groups["ruleValues"].Value;
+            WriteValue(writer, RuleFirstValueRegex.Match(rawRuleValues));
+            foreach (Match valueMatch in RuleValueRegex.Matches(rawRuleValues))
+            {
+                writer.WriteLine(",");
+                WriteValue(writer, valueMatch);
+            }
+
+            writer.WriteLine();
+        }
+
+        private static string GetDataLocation(string[] args)
+        {
+            if (args.Any())
+            {
+                Console.WriteLine($"Using data location from argument ({args[0]})");
+                return args[0];
+            }
+
+            Console.WriteLine($"No data location passed. Using default data location ({DefaultDataLocation})");
+            return DefaultDataLocation;
+        }
+
+        private static void WriteValue(StreamWriter writer, Match valueMatch)
+        {
+            string documentation = EscapeDocumentation(valueMatch.Groups["documentation"].Value.TrimStart());
+            if (string.IsNullOrEmpty(documentation))
+            {
+                writer.WriteLine($"\"{valueMatch.Groups["valueName"].Value}\"");
+                return;
+            }
+
+            writer.WriteLine("        {");
+            writer.WriteLine("          \"type\": \"constant\",");
+            writer.WriteLine($"          \"value\": \"{valueMatch.Groups["valueName"].Value}\",");
+            writer.WriteLine($"          \"documentation\": \"{documentation}\"");
+            writer.Write("        }");
+        }
+
+        private static void WriteKey(StreamWriter writer, Match optionMatch)
+        {
+            if (!optionMatch.Groups["ruleDocumentation"].Success)
+            {
+                writer.WriteLine($"    \"key\": \"{optionMatch.Groups["ruleName"]}\",");
+                return;
+            }
+
+            writer.WriteLine("    \"key\": {");
+            writer.WriteLine("        \"type\": \"constant\",");
+            writer.WriteLine($"        \"value\": \"{optionMatch.Groups["ruleName"]}\",");
+            writer.WriteLine(
+                $"        \"documentation\": \"{EscapeDocumentation(optionMatch.Groups["ruleDocumentation"].Value)}\"");
+            writer.WriteLine("    },");
+        }
+
+        private static string EscapeDocumentation(string documentation) => TagRegex.Replace(documentation, "");
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+#EditorConfig metadata generator
+
+This project can be used to generate EditorConfig metadata for IntelliJ.
+
+###The problem:
+Microsoft released [a new set of EditorConfig rules](https://docs.microsoft.com/en-us/visualstudio/ide/editorconfig-formatting-conventions).
+For IntelliJ-based IDEs to support them, metadata describing those keys and values needs to be provided.
+Due to sheer numbers of the new rules, creating the metadata manually is tiresome.
+
+###The solution:
+Download the page and use regular expressions to extract rules from it.
+Then generate the metadata based on that.
+
+###Usage:
+1. Open the [page with rule descriptions](https://docs.microsoft.com/en-us/visualstudio/ide/editorconfig-formatting-conventions);
+2. Right-click it and select `View page source`;
+3. Copy all the code;
+4. Create a file called `Data.html` on the disk and paste the code into it;
+5. Build and run the project, passing a path to the folder containing `Data.html` as an argument;
+6. Open `Output.json` file created near `Data.html`.
+
+The generated metadata can now be moved into the proper place in IntelliJ EditorConfig plugin.
+
+Kirill Glazyrin, JetBrains, 2020
